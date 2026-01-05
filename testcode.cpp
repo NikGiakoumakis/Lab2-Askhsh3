318aa63eb1977082f7c8e64972f3a9e56f2dfa91
Nikiforos Marios G. <nikiforosmercy13@Gmail.com>
2026-01-05 16:41:07 +0200

    Add project files.

---
+++ testcode.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!\n"); // Yo
+	
+	
+	printf("This is a test code.\n"); /*This is a multi line comment abouy my
+									  life and how aweosme it is*/
+	return 0;
+}
